a5fdba935d0f9061e88dbd47127959372ae91cfa
sunho-lee <sh112123@naver.com>
2023-11-08 20:25:53 +0900

    [기능] Logger구현체 logback에서 log4j2로 변경
    1. spring web 기본 로거인 logback 종속성 제외
    2. log4j2 종속성 추가
    3. @Slf4j 사용을 위해 lombok 종속성 추가
    4. 앱 실행 시 현재 로거 출력

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,16 +18,32 @@ repositories {
 }
 val asciidoctorExt: Configuration by configurations.creating
 
+
+configurations {
+    all {
+        exclude("org.springframework.boot","spring-boot-starter-logging")
+    }
+}
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-graphql")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-hateoas")
     implementation("org.springframework.boot:spring-boot-starter-validation")
+
+    implementation("org.springframework.boot:spring-boot-starter-log4j2")
+    //modules {
+    //    module("org.springframework.boot:spring-boot-starter-logging") {
+    //        replacedBy("org.springframework.boot:spring-boot-starter-log4j2", "Use Log4j2 instead of Logback")
+    //    }
+    //}
     //implementation("org.springframework.boot:spring-boot-starter-security")
 
     implementation("org.mapstruct:mapstruct:1.5.5.Final")
     annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
+    compileOnly("org.projectlombok:lombok:1.18.30")
+    annotationProcessor("org.projectlombok:lombok:1.18.30")
 
     runtimeOnly("com.mysql:mysql-connector-j")
 
--- src/main/java/com/example/controllerexample/ControllerExampleApplication.java
+++ src/main/java/com/example/controllerexample/ControllerExampleApplication.java
@@ -1,13 +1,16 @@
 package com.example.controllerexample;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class ControllerExampleApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ControllerExampleApplication.class, args);
+        log.info("logger is {}", log.getClass().getSimpleName());
     }
 
 }
