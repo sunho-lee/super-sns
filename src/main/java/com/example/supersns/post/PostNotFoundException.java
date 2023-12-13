628b976f07ff0de26a492f7d04f1df50aec5a980
sunho-lee <sh112123@naver.com>
2023-12-13 14:44:56 +0900

    [기타] git actions test 8

--- src/main/java/com/example/supersns/post/PostNotFoundException.java
+++ src/main/java/com/example/supersns/post/PostNotFoundException.java
@@ -6,10 +6,10 @@ import org.springframework.web.ErrorResponseException;
 
 public class PostNotFoundException extends ErrorResponseException {
     public PostNotFoundException(Long postId) {
-        super(HttpStatus.NOT_FOUND, asProblemDetail("Post with id: " + postId + " not found" ), null);
+        super(HttpStatus.NOT_FOUND, asProblemDetail("Post with id: " + postId + " not found"), null);
     }
 
-    private static ProblemDetail asProblemDetail(String message){
+    private static ProblemDetail asProblemDetail(String message) {
         ProblemDetail pd = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, message);
         pd.setTitle("Post not found.");
         return pd;
